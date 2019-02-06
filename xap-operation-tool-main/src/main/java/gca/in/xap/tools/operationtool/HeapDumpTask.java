3a0ce4d07dfb7e496d2809c0bc8a23c910970e7a
Yann ROBERT [347619cd-ee19-4e47-8065-6b37c23fb466] <yann.robert-prestataire@ca-ps.com>
2019-02-06 10:57:25 +0100

    added heapdump feature

---
+++ xap-operation-tool-main/src/main/java/gca/in/xap/tools/operationtool/HeapDumpTask.java
@@ -0,0 +1,37 @@
+package gca.in.xap.tools.operationtool;
+
+import gca.in.xap.tools.operationtool.helper.UserDetailsConfigFactory;
+import gca.in.xap.tools.operationtool.helper.XapHelper;
+import org.openspaces.admin.pu.config.UserDetailsConfig;
+
+import java.util.concurrent.TimeUnit;
+
+public class HeapDumpTask {
+
+    private final UserDetailsConfigFactory userDetailsConfigFactory = new UserDetailsConfigFactory();
+
+    public void doHeapDump(ApplicationArguments applicationArguments) {
+        UserDetailsConfig userDetails = userDetailsConfigFactory.createFromUrlEncodedValue(
+                applicationArguments.username,
+                applicationArguments.password
+        );
+
+        XapHelper xapHelper = new XapHelper.Builder()
+                .locators(applicationArguments.locators)
+                .groups(applicationArguments.groups)
+                .timeout(applicationArguments.timeoutDuration)
+                .userDetails(userDetails)
+                .create();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        xapHelper.printReportOnContainersAndProcessingUnits();
+
+        xapHelper.generateHeapDumpOnEachGsc();
+    }
+
+}
