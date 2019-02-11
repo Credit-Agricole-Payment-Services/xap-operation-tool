d9b59b0578fcb9e9f22eca8a1deb67c4f433fdaf
Yann ROBERT [347619cd-ee19-4e47-8065-6b37c23fb466] <yann.robert-prestataire@ca-ps.com>
2019-02-11 15:12:50 +0100

    improved support for env and system property logging and usage

--- xap-operation-tool-main/src/main/java/gca/in/xap/tools/operationtool/ApplicationArguments.java
+++ xap-operation-tool-main/src/main/java/gca/in/xap/tools/operationtool/ApplicationArguments.java
@@ -1,5 +1,6 @@
 package gca.in.xap.tools.operationtool;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetAddress;
@@ -7,7 +8,6 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.time.Duration;
 import java.util.*;
-import java.util.logging.Level;
 
 @Slf4j
 public class ApplicationArguments {
@@ -17,34 +17,28 @@ public class ApplicationArguments {
 
 	private static final String PROP_LOOKUP_GROUPS = "lookup.groups";
 	private static final String PROP_LOOKUP_GROUPS_ENV = "XAP_LOOKUP_GROUPS";
-	private static final String PROP_LOOKUP_GROUPS_DEFAULT = System.getenv().getOrDefault(PROP_LOOKUP_GROUPS_ENV, "xap");
 
 	private static final String PROP_LOOKUP_LOCATORS = "lookup.locators";
 	private static final String PROP_LOOKUP_LOCATORS_ENV = "XAP_LOOKUP_LOCATORS";
-	private static final String PROP_LOOKUP_LOCATORS_DEFAULT = System.getenv().getOrDefault(PROP_LOOKUP_LOCATORS_ENV, "localhost");
-
-	private static final String PROP_LOG_LEVEL_ROOT = "log.level.root";
-	private static final String PROP_LOG_LEVEL_ROOT_DEFAULT = Level.INFO.getName();
 
 	private static final String PROP_TIMEOUT = "timeout";
 	private static final String PROP_TIMEOUT_DEFAULT = "PT1M";
 
 	private static final String USAGE = "args: <zipFile> (<propsFile>)"
 			+ "\nAvailable system properties:"
-			+ "\n -D" + PROP_LOOKUP_GROUPS + " (comma separated multi-values. Default value (cf. env:" + PROP_LOOKUP_GROUPS_ENV + ") : " + PROP_LOOKUP_GROUPS_DEFAULT + ")"
-			+ "\n -D" + PROP_LOOKUP_LOCATORS + " (comma separated multi-values. Default (cf. env:" + PROP_LOOKUP_LOCATORS_ENV + ") : " + PROP_LOOKUP_LOCATORS_DEFAULT + ")"
+			+ "\n -D" + PROP_LOOKUP_GROUPS + " (comma separated multi-values. Default value (cf. env:" + PROP_LOOKUP_GROUPS_ENV + ") : " + findLookupGroups() + ")"
+			+ "\n -D" + PROP_LOOKUP_LOCATORS + " (comma separated multi-values. Default (cf. env:" + PROP_LOOKUP_LOCATORS_ENV + ") : " + findLookupGroups() + ")"
 			+ "\n -D" + PROP_CREDENTIAL_USERNAME + " (URL Encoded value)"
 			+ "\n -D" + PROP_CREDENTIAL_SECRET + " (URL Encoded value)"
-			+ "\n -D" + PROP_LOG_LEVEL_ROOT + " (Default value: " + PROP_LOG_LEVEL_ROOT_DEFAULT + ")"
 			+ "\n -D" + PROP_TIMEOUT + " (ISO-8601 Duration. Default value: " + PROP_TIMEOUT_DEFAULT + ")";
 
 	final String username = System.getProperty(PROP_CREDENTIAL_USERNAME);
 
 	final String password = System.getProperty(PROP_CREDENTIAL_SECRET, "");
 
-	final List<String> locators = Arrays.asList(System.getProperty(PROP_LOOKUP_LOCATORS, PROP_LOOKUP_LOCATORS_DEFAULT).split(","));
+	final List<String> locators = findLookupLocators();
 
-	final List<String> groups = Arrays.asList(System.getProperty(PROP_LOOKUP_GROUPS, PROP_LOOKUP_GROUPS_DEFAULT).split(","));
+	final List<String> groups = findLookupGroups();
 
 	final Duration timeoutDuration = Duration.parse(System.getProperty(PROP_TIMEOUT, PROP_TIMEOUT_DEFAULT));
 
@@ -83,6 +77,26 @@ public class ApplicationArguments {
 			throw new RuntimeException(e);
 		}
 		log.info("All network addresses : {}", addresses);
+	}
+
+	static List<String> findLookupGroups() {
+		return findProperty(PROP_LOOKUP_GROUPS_ENV, PROP_LOOKUP_GROUPS, "xap");
+	}
+
+	static List<String> findLookupLocators() {
+		return findProperty(PROP_LOOKUP_LOCATORS_ENV, PROP_LOOKUP_LOCATORS, "localhost");
+	}
+
+	static List<String> findProperty(@NonNull String envPropertyName, @NonNull String systemPropertyName, @NonNull String defaultValue) {
+		String envValue = System.getenv().get(envPropertyName);
+		log.info("Value for {} ENV property is : {}", envPropertyName, envValue);
+		String systemPropertyValue = System.getProperty(systemPropertyName);
+		log.info("Value for {} System property is : {}", systemPropertyName, systemPropertyValue);
+		String value = (systemPropertyValue != null) ? systemPropertyValue : envValue;
+		if (value == null) {
+			value = defaultValue;
+		}
+		return Arrays.asList(value.split(","));
 	}
 
 }
