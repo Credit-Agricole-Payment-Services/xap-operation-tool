d7859f0f8bfba47614a011363f9f8ba33a806a5c
Bastien Jansen <bastien.jansen@ca-ps.com>
2019-03-13 11:56:55 +0100

    chore(maven): évite de spécifier dans les scripts sh la version du projet

--- xap-operation-tool-dist/src/main/bin/restart-managers.sh
+++ xap-operation-tool-dist/src/main/bin/restart-managers.sh
@@ -19,6 +19,6 @@ CLASSPATH=${CLASSPATH}:$(find "${LIB_DIR}" -name '*.jar' | tr '\n' ':')
 
 OPERATION=restart-managers
 
-time java -Xms1G -Xmx1G -Dcom.gs.logging.disabled=true -cp "${CLASSPATH}" -jar $LIB_DIR/xap-operation-tool-main-1.1.0-SNAPSHOT.jar $OPERATION "$@"
+time java -Xms1G -Xmx1G -Dcom.gs.logging.disabled=true -cp "${CLASSPATH}" -jar $LIB_DIR/xap-operation-tool-main-${project.version}.jar $OPERATION "$@"
 
 echo "Script $0 finished successfully"
