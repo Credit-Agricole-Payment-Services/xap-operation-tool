40bc10a4fbc9d64b80c1715d2fc81d87e09145ba
Yann ROBERT [347619cd-ee19-4e47-8065-6b37c23fb466] <yann.robert-prestataire@ca-ps.com>
2019-04-08 16:12:34 +0200

    improved chance to log uncaughtException by using a sleep before exit

--- xap-operation-tool-main/src/main/java/gca/in/xap/tools/operationtool/Tool.java
+++ xap-operation-tool-main/src/main/java/gca/in/xap/tools/operationtool/Tool.java
@@ -6,23 +6,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @SpringBootApplication()
 public class Tool {
 
 	public static void main(String[] args) {
+		Thread.setDefaultUncaughtExceptionHandler(Tool::uncaughtException);
 		SysOutOverSLF4J.sendSystemOutAndErrToSLF4J();
 		BuildInfo.printBuildInformation();
-		Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-			log.error("Uncaught Exception in Thread {}", t.getName(), e);
-			System.exit(1);
-		});
 		System.setProperty("com.gigaspaces.unicast.interval", "1000,1000");
 		log.info("args = {}", Arrays.asList(args));
 
+		// start the actual application using Spring Boot ...
 		SpringApplication.run(Tool.class, args);
 	}
 
+	private static void uncaughtException(Thread t, Throwable e) {
+		log.error("Uncaught Exception in Thread {}", t.getName(), e);
+		try {
+			TimeUnit.MILLISECONDS.sleep(100);
+		} catch (InterruptedException e1) {
+			throw new RuntimeException(e);
+		}
+		System.exit(1);
+	}
 }
 
