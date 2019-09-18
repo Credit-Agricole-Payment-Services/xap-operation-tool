090f43cbe0e90d966b3582822a2ebf62ccf86222
Yann ROBERT [347619cd-ee19-4e47-8065-6b37c23fb466] <yann.robert-prestataire@ca-ps.com>
2019-09-18 15:33:57 +0200

    fixed filtering @Option to split items on ','

--- xap-operation-tool-main/src/main/java/gca/in/xap/tools/operationtool/commandoptions/ContainersMachinesFilterOptions.java
+++ xap-operation-tool-main/src/main/java/gca/in/xap/tools/operationtool/commandoptions/ContainersMachinesFilterOptions.java
@@ -8,10 +8,10 @@ import java.util.function.Predicate;
 
 public class ContainersMachinesFilterOptions {
 
-	@CommandLine.Option(names = {"--machinesIncludes"}, description = "List of names of the Machines to include. If you only want to act on a subset of the Machines, you can specify 1 or more machine name.")
+	@CommandLine.Option(names = {"--machinesIncludes"}, split = ",", description = "List of names of the Machines to include. If you only want to act on a subset of the Machines, you can specify 1 or more machine name.")
 	public List<String> machinesIncludes;
 
-	@CommandLine.Option(names = {"--machinesExcludes"}, description = "List of names of the Machines to exclude. If you only want to act on a subset of the Machines, you can specify 1 or more machine name.")
+	@CommandLine.Option(names = {"--machinesExcludes"}, split = ",", description = "List of names of the Machines to exclude. If you only want to act on a subset of the Machines, you can specify 1 or more machine name.")
 	public List<String> machinesExcludes;
 
 	public Predicate<GridServiceContainer> toPredicate() {
