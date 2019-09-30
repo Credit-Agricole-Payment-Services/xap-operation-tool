9a685cac445e2f9a95e8f5207ba41a888dbd8c25
Yann ROBERT [347619cd-ee19-4e47-8065-6b37c23fb466] <yann.robert-prestataire@ca-ps.com>
2019-09-30 16:12:41 +0200

    fixed too verbose logging

--- xap-operation-tool-main/src/main/java/gca/in/xap/tools/operationtool/predicates/container/StatefulBackupsOnlyPredicate.java
+++ xap-operation-tool-main/src/main/java/gca/in/xap/tools/operationtool/predicates/container/StatefulBackupsOnlyPredicate.java
@@ -8,7 +8,7 @@ public class StatefulBackupsOnlyPredicate extends AbstractStatefullOnlyPredicate
 	@Override
 	protected boolean test(String gscId, boolean containsStatefulBackupPU, boolean containsStatefulPrimaryPU) {
 		boolean result = containsStatefulBackupPU & !containsStatefulPrimaryPU;
-		log.info("GSC {} : containsStatefulBackupPU = {}, containsStatefulPrimaryPU = {}, result = {}", gscId, containsStatefulBackupPU, containsStatefulPrimaryPU, result);
+		log.debug("GSC {} : containsStatefulBackupPU = {}, containsStatefulPrimaryPU = {}, result = {}", gscId, containsStatefulBackupPU, containsStatefulPrimaryPU, result);
 		return result;
 	}
 
